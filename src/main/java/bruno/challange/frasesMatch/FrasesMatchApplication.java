7f64ff45723872b7deae4887ea22fa41da9a7d6f
BrunoDMO <brunonaw41@gmail.com>
2024-02-29 17:06:00 -0300

    fisrt commit

---
+++ src/main/java/bruno/challange/frasesMatch/FrasesMatchApplication.java
@@ -0,0 +1,12 @@
+package bruno.challange.frasesMatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrasesMatchApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(FrasesMatchApplication.class, args);
+	}
+
+}
